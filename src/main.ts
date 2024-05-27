4bb0534b9294f2c0d29ac7cd2d97c61eb379601e
Dan-MK9 <166556625+Dan-MK9@users.noreply.github.com>
2024-05-27 17:23:57 -0300

    feat: Colisão

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Actor, CollisionType, Color, Engine, vec } from "excalibur"
+import { Actor, CollisionType, Color, Engine, Font, Text, vec } from "excalibur"
 
 // 1 - Criar uma instancia de Engine, que representa o jogo
 const game = new Engine({
@@ -10,9 +10,10 @@ const game = new Engine({
 const barra = new Actor({
 	x: 150,
 	y: game.drawHeight - 40, // game.drawHeight
-	width: 200,
+	width: 100,
 	height: 20,
-	color: Color.Chartreuse
+	color: Color.Chartreuse,
+	name: "BarraJogador"
 })
 
 // Define o tipo de colisão da barra
@@ -40,7 +41,7 @@ const bolinha = new Actor({
 bolinha.body.collisionType = CollisionType.Passive
 
 // 5 - Criar movimentação da bolinha
-const velocidadeBolinha = vec(100, 100)
+const velocidadeBolinha = vec(400, 400)
 
 // Apos 1 segundo (1000 ms), define a velocidade da bolinha em x = 100 = e y = 100
 setTimeout(() => {
@@ -50,7 +51,7 @@ setTimeout(() => {
 // 6 - Fazer bolinha rebater na parede
 bolinha.on("postupdate", () => {
 	// Se a bolinha colidir com o lado esquerdo
-	if (bolinha.pos.x <bolinha .width / 2) {
+	if (bolinha.pos.x < bolinha.width / 2) {
 		bolinha.vel.x = velocidadeBolinha.x
 	}
 
@@ -73,15 +74,15 @@ game.add(bolinha)
 
 // 7 - Criar blocos
 // Configuração de tamanho e espaçamento dos blocos
-const padding = 20 
+const padding = 20
 
-const xoffset = 65 
-const yoffset = 20 
+const xoffset = 65
+const yoffset = 20
 
 const colunas = 5
-const linhas = 3 
+const linhas = 3
 
-const corBloco = [Color.Violet, Color.Orange, Color.Yellow]
+const corBloco = [Color.Red, Color.Orange, Color.Yellow]
 
 const larguraBloco = (game.drawWidth / colunas) - padding - (padding / colunas)
 // const larguraBloco = 136
@@ -89,6 +90,91 @@ const alturaBloco = 30
 
 const listaBlocos: Actor[] = []
 
+// Renderização dos bloquinhos
+
+for (let j = 0; j < linhas; j++) {
+	// Renderiza 5 bloquinhos
+	for (let i = 0; i < colunas; i++) {
+		listaBlocos.push(
+			new Actor({
+				x: xoffset + i * (larguraBloco + padding) + padding,
+				y: yoffset + j * (alturaBloco + padding) + padding,
+				width: larguraBloco,
+				height: alturaBloco,
+				color: corBloco[j]
+			})
+		)
+	}
+}
+
+listaBlocos.forEach(bloco => {
+	// define o tipo de colisor de cada bloco
+	bloco.body.collisionType = CollisionType.Active
+
+	// adiciona cada bloco no game
+	game.add(bloco)
+})
+
+// Adicionando pontuação
+let pontos = 0
+
+const textoPontos = new Text({
+	text: "Hello World",
+	font: new Font({size: 20})
+})
+
+const objetoTexto = new Actor({
+	x: game.drawWidth - 80,
+	y: game.drawHeight - 15
+})
+
+objetoTexto.graphics.use(textoPontos)
+
+game.add(objetoTexto)
+
+let colidindo: boolean = false
+
+
+bolinha.on("collisionstart", (event) => {
+	// Verificar se a bolinha colidiu com algum bloco destrutivel
+	console.log("Colidiu com ", event.other.name);
+
+	// Se o elemento colidido for um bloco da lista de blocos (destruidos)
+	if (listaBlocos.includes(event.other) ) {
+		// Destruir o bloco colidido
+		event.other.kill()
+	}
+
+	// Rebater a bolinha - Inverter as direções
+	let interseccao = event.contact.mtv.normalize()
+
+	// Se não esta colidindo
+	// !colidindo -> colidindo == false
+	if (!colidindo) {
+		colidindo = true
+
+		// Interseccao.x e interseccao.y
+		// O maior representsa o eixo onde houve o contato
+		if( Math.abs(interseccao.x) > Math.abs(interseccao.y)) {
+			// bolinha.vel.x = -bolinha.vel.x
+			// bolinha.vel.x *= -1
+			bolinha.vel.x = bolinha.vel.x * -1
+		} else {
+			// bolinha.vel.y = -bolinha.vel.y
+			// bolinha.vel.y *= -1
+			bolinha.vel.y = bolinha.vel.y * -1
+		}
+	}
+}) 
+
+bolinha.on("collisionend", () => {
+	colidindo = false
+})
+
+bolinha.on("exitviewport", () => {
+	alert("Faleceu")
+	window.location.reload()
+})
 
 // Inicia o game
 game.start()
